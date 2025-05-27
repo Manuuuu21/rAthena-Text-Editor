5b8c9080ee881fec19c24cc963aa4034fb0d06cb
Manuuuu21 <151635459+Manuuuu21@users.noreply.github.com>
2025-05-27 21:31:43 +0800

    Add files via upload

--- library/rathena-highlight.js
+++ library/rathena-highlight.js
@@ -3,17 +3,17 @@ ace.define("ace/mode/rathena_highlight_rules", ["require", "exports", "ace/lib/o
   const TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
 
   const RathenaHighlightRules = function() {
-    const itemCommands = "getitem|getitem2|getitem3|getinventorylist|getitemname|clearitem|equip|autoequip|itemskill|getarraysize|rentitem|rentitem2|rentitem3|rentitem4|delitem|delitem2|delitem3|delitem4|countitem|countitem2|countitem3|countitem4|cartcountitem";
-    const functionCommands = "callfunc|sleep|strcharinfo|input|select|rand|unitwarp|getfreecell|initnpctimer|getnpcid|duplicate";
-    const variableCommands = "set|setarray|copyarray|cleararray|setrandomoption";
-    const controlStructures = "if|else|switch|case|default|while|for|break|end|script|goto";
+    const itemCommands = "getitem|getitem2|getitem3|getinventorylist|getitemname|clearitem|equip|autoequip|itemskill|getarraysize|rentitem|rentitem2|rentitem3|rentitem4|delitem|delitem2|delitem3|delitem4|countitem|countitem2|countitem3|countitem4|cartcountitem|getitembound|getitembound2|getitembound3";
+    const functionCommands = "callfunc|sleep|strcharinfo|input|select|rand|unitwarp|getfreecell|initnpctimer|getnpcid|duplicate|readparam";
+    const variableCommands = "set|setarray|copyarray|cleararray|setrandomoption|bStr|bAgi|bVit|bInt|bDex|bLuk|Zeny";
+    const controlStructures = "if|else|switch|case|default|while|for|break|end|script|goto|function";
     const dialogCommands = "mes|next|close|close2|npctalk|waitingroom|delwaitingroom|menu|announce|message";
-    const systemCommands = "recalculatestat|jobcanentermap|freeloop|get_revision|get_githash";
+    const systemCommands = "recalculatestat|jobcanentermap|freeloop|get_revision|get_githash|getbattleflag";
     const mapCommands = "pvpon|pvpoff|navigateto|mapflag|warpwaitingpc";
     const channelCommands = "channel_chat|channel_ban|channel_kick|channel_unban|channel_delete|channel_setgroup|channel_setgroup2";
     const petCommands = "petloot|petrecovery|petskillbonus|petskillsupport|petskillattack|petskillattack2";
     const scCommands = "sc_start|sc_start2|sc_start4|sc_end|sc_end_class";
-    const bonusCommands = "bonus|bonus2|bonus3|bonus4|bonus5|autobonus|autobonus2|autobonus3";
+    const bonusCommands = "bonus|bonus2|bonus3|bonus4|bonus5|autobonus|autobonus2|autobonus3|statusup2";
     const atCommands = "atcommand|charcommand|bindatcmd|unbindatcmd|useatcmd";
     const mapNames = "prontera|morocc|geffen|gef_fild10|payon|pay_arche|alberta|izlude|aldebaran|xmas|comodo|yuno|amatsu|gonryun|umbala|niflheim|louyang|ayothaya|jawaii|einbroch|lighthalzen|einbech|hugel|rachel|veins|moscovia|mid_camp|manuk|splendide|brasilis|dicastes01|mora|dewata|malangdo|malaya|eclage|lasagna|new_1-1|new_1-2|turbo_room"; // <-- Extend this list
 
